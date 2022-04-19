45c0e0af2916103337c2f133bf8b83e242823c72
Tracy104351 <t110ac2001@ntut.org.tw>
2022-04-19 23:06:18 +0800

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,418 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Console.WriteLine("Hello, World!");
+string message = "123";
+message = "456";
+Console.WriteLine(message);
+String ID ="110AC2001";
+float currency = 0.01f;
+int year = 18;
+int height ;
+bool male = true;
+Console.WriteLine("Hello, World!");//好ㄟ!
+/*ㄟ喔
+喔ㄟ*/
+/*Console.WriteLine("i am a girl"+male);
+height = 157;
+Console.WriteLine("ColdMoon!");
+Console.WriteLine("My age:"+year);
+Console.WriteLine("My Id:"+ID);
+Console.WriteLine("My height:"+height);
+Console.WriteLine("currency1:"+currency);
+currency = currency +1 ;
+Console.WriteLine("currency1:"+currency);
+
+int a, b; 
+a = 2;
+b = a;
+a = 3;
+Console.WriteLine(a);
+Console.WriteLine(b);
+
+Console.WriteLine(1 + 2);
+int c;
+c = 1 + 2;
+Console.WriteLine(c + 1);
+
+int d = 2;
+Console.WriteLine(c + d);
+
+c = c + d;
+Console.WriteLine(c + d);
+
+Console.WriteLine("------------");
+int e = 3;
+Console.WriteLine(e);
+Console.WriteLine(e++);
+Console.WriteLine(e);
+
+int f = 3;
+Console.WriteLine(f);
+Console.WriteLine(++f);
+Console.WriteLine(f);
+
+Console.WriteLine("------------");
+int g = 3;
+Console.WriteLine(g);
+Console.WriteLine(g--);
+Console.WriteLine(g);
+
+int h = 3;
+Console.WriteLine(h);
+Console.WriteLine(--h);
+Console.WriteLine(h);
+
+Console.WriteLine("------------");
+int i;
+i = 2 + 2 * 2 ;
+Console.WriteLine(i);
+
+int j;
+j = (2 + 2) * 2 ;
+Console.WriteLine(j);
+
+Console.WriteLine("------------");
+int num = 214;
+float bignum = num;
+Console.WriteLine(bignum);
+
+Console.WriteLine("------------");
+int k = 5;
+string l = k + "";  
+Console.WriteLine(k);
+
+Console.WriteLine("------------");
+float x = 1.2345f;
+int n;
+n = (int)x;
+System.Console.WriteLine(n);
+
+Console.WriteLine("------------");
+int o = 5;
+int p = 5;
+Console.WriteLine(o == p);
+Console.WriteLine(o != p);
+
+Console.WriteLine("------------");
+int q = 5;
+int r = 5;
+Console.WriteLine(5 > 3);
+Console.WriteLine(5 >= 3);
+Console.WriteLine(q >= r);
+Console.WriteLine(5 < 3);
+Console.WriteLine(5 <= 3);
+Console.WriteLine(q <= 5);
+
+Console.WriteLine("------------");
+Console.WriteLine(true & true);
+Console.WriteLine(true & false);
+Console.WriteLine(false & true);
+Console.WriteLine(false & false);
+
+Console.WriteLine(5 > 3 & q == r);
+Console.WriteLine(5 > 3 & q != r);
+
+Console.WriteLine(false & q++ > 0);
+Console.WriteLine(q);
+Console.WriteLine(false && r++ > 0);
+Console.WriteLine(r);
+
+Console.WriteLine(!false);
+Console.WriteLine(!true);
+
+Console.WriteLine(!(3 > q));
+Console.WriteLine(!(r > 3));
+
+Console.WriteLine("------------");
+Console.WriteLine(5 < 3 || q ==r );
+Console.WriteLine(5 < 3 || q !=r );
+
+Console.WriteLine("------------");
+Console.WriteLine(q++ < r || q > r );
+Console.WriteLine(q > r ^ q == r );
+Console.WriteLine((q > r && q > r)  ||　q > r);
+
+Console.WriteLine("------------");
+int s = 5;
+int t = 6;
+if (s + t > 10){Console.WriteLine("The answer is greater than 10");
+}
+
+Console.WriteLine("------------");
+int u =5;
+int v =4;
+if(u + v > 15)
+{
+Console.WriteLine("The answer is greater than 15");
+}
+else if (u + v > 10)
+{
+Console.WriteLine("The answer is between 15 and 10");
+}
+else 
+{
+Console.WriteLine("The answer is not greater than 10");
+}
+
+
+Console.WriteLine("------------");
+String food;
+Random random = new Random();
+int w = random.Next(0,100);
+Console.WriteLine(w);
+
+Console.WriteLine("We have remen, spaghetti and salad. Which do you like?");
+food = Console.ReadLine();
+
+switch (food){
+    
+case "ramen":
+    Console.WriteLine("It cost you 200.");
+    break;
+
+case "spaghetti":
+    Console.WriteLine("It cost you 180.");
+    break;
+
+case "salad":
+    Console.WriteLine("It cost you 60.");
+    break;
+
+default:
+    Console.WriteLine("Sorry ,we do not sell" + food +".");
+    break;
+}
+*/
+
+/*Console.WriteLine ("count to 5(do...while loop)");
+int y = 0;
+do
+{
+    Console.WriteLine("吃冷月");
+    y++;
+}   while(y <= 3);
+    
+
+Console.WriteLine ("count to 5(do...while loop)");
+int z = 0;
+while(z <= 5)
+{
+    Console.WriteLine("吃薐羽");
+    z++;
+}   */
+    /*
+    Console.WriteLine ("Nested loop: Square");
+    for (int b = 0; b < 9; b++)
+    {
+        for (int c = 0; c < 9; c++)
+        {
+             Console.Write ("*");
+        }
+
+        Console.WriteLine ("");
+    }
+
+    Console.WriteLine ("Nested loop: Square");
+    for (int b = 0; b < 9; b++)
+    {
+        for (int c = 0; c < b; c++)
+        {
+             Console.Write ("*");
+        }
+
+        Console.WriteLine ("");
+    }
+
+     Console.WriteLine ("------------------");
+    Console.WriteLine ("Nested loop: Square");
+    for (int b = 0; b < 9; b++)
+    {
+        for (int c = 0; c < b; c++)
+        {
+             Console.Write ("*");
+        }
+
+        Console.WriteLine ("");
+    }*/
+
+    /*for(int a =0; a<9; a++)
+    {
+        for(int b =9; a<b; b--)
+        {
+        Console.Write("*");
+        }
+        Console.WriteLine(" ");
+    }*/
+
+
+ /*   
+   Console.WriteLine("菱形");
+    for(int a =0; a<4; a++)
+    {
+        for(int b =3; a<b; b--)
+        {
+        Console.Write(" ");
+        }
+
+        for(int c =0; c<(a*2+1); c++)
+        {
+        Console.Write("*");
+        }
+        
+         Console.WriteLine(" ");
+    }
+    
+   for(int d =1; d<4; d++)
+    {
+        for(int f =0; f<d; f++)
+        {
+        Console.Write(" ");
+        }
+        
+        for(int g =7; g>d*2 ; g--)
+        {
+            
+        Console.Write("*");
+        
+        }
+        Console.WriteLine(" ");
+         
+    }
+*/
+
+   /*   Console.WriteLine("test2");
+    for(int d =0; d<6; d++)
+    {
+        for(int f =0; f<d; f++)
+        {
+        Console.Write(" ");
+        }
+
+        for(int g =6; g>d ; g--)
+        {
+        Console.Write("*");
+        }
+        
+         Console.WriteLine(" ");
+    }
+    }*/
+/*
+    Console.WriteLine("test2");
+    for(int d =0; d<6; d++)
+    {
+        for(int f =0; f<d; f++)
+        {
+        Console.Write(" ");
+        }
+
+        for(int g =6; g>d ; g--)
+        {
+        Console.Write("*");
+        }
+        
+         Console.WriteLine(" ");
+    }
+*/
+    /*int a =8;
+    int b =18;
+    int c =3;
+    int d =0;
+    d=(a*b)%c;
+    Console.WriteLine(d);*/
+
+
+
+
+
+   /* int a =8;
+    Console.WriteLine(a);
+    Console.WriteLine(++a);
+    Console.WriteLine(a);
+
+    int b =8;
+    Console.WriteLine(b);
+    Console.WriteLine(b++);
+    Console.WriteLine(b);*/
+
+bool Start=true;  
+bool Show=false;
+
+string Name ="";
+string Gender="";
+string Birth="";
+string StudentNum="";
+
+string ChangeThis="";
+string Correct="";
+
+while(Start==true)
+{
+Console.WriteLine("請輸入姓名:");
+Name = Console.ReadLine();
+
+Console.WriteLine("請輸入性別:");
+Gender = Console.ReadLine();
+
+Console.WriteLine("請輸入出生年月日:");
+Birth = Console.ReadLine();
+
+Console.WriteLine("請輸入學號:");
+StudentNum = Console.ReadLine();
+
+  while(Show==true)
+    { 
+    Console.WriteLine("----------------------------------------------------------------------------------------------");
+    Console.WriteLine("你的名字是:"+Name+"  你的性別是:"+Gender+"  你的出生日期是:"+Birth+"  你的學號是:"+StudentNum);
+    Console.WriteLine("請問資料是否正確?1為正確 2為錯誤 其他則跳回");
+    Correct = Console.ReadLine();
+        if (Correct=="1")
+        {
+         Show=false;
+         Start=false;
+         Console.WriteLine("資料建構完成!");
+        }
+        else if (Correct=="2")
+        {
+            Console.WriteLine("請問要改哪筆資料?輸入:1名字 2性別 3生日 4學號 ");
+            ChangeThis=Console.ReadLine();
+            switch(ChangeThis)
+            {
+                case"1":
+                Console.WriteLine("請輸入新的姓名:");
+                Name = Console.ReadLine();
+                Console.WriteLine("資料建構完成!");
+                Show=false;
+                ChangeThis="";
+                break;
+
+                case"2":
+                Console.WriteLine("請輸入新的性別:");
+                Gender = Console.ReadLine();
+                Console.WriteLine("資料建構完成!");
+                ChangeThis="";
+                break;
+
+                case"3":
+                Console.WriteLine("請輸入新的生日:");
+                Birth = Console.ReadLine();
+                Console.WriteLine("資料建構完成!");
+                ChangeThis="";
+                break;
+
+                case"4":
+                Console.WriteLine("請輸入新的學號:");
+                StudentNum = Console.ReadLine();
+                Console.WriteLine("資料建構完成!");
+                ChangeThis="";
+                break;
+
+                default:
+                ChangeThis="";
+                break;
+                
+            }
+            
+            
+         }
+    }
+
+ } 
